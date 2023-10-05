bbe356eb111300b03b58e9832609127cc6238f0b
stemisruler <130126153+stemisruler@users.noreply.github.com>
2023-10-05 09:56:54 +0900

    UI 개선 및 작은 오류 해결
    1.subHeader border 삭제
    
    2.http://www.goodmorningcc.com/news/photo/202208/274736_297685_2113.jpg'  이미지 경로 https로 수정. https에서 http 요소를 불러와서 주의 메시지 뜸.
    
    3.sub1 검색창에서 엔터 누르면 input 기본 동작 때문에 이상한 페이지로 가서 404 page 뜨는 거 방지하기 위해 form 태그 안에 handleFormSubmit 함수 넣고, 그 함수 안의 기능으로 preventDefault 넣음.

--- src/component/sub_component/SubHeader.js
+++ src/component/sub_component/SubHeader.js
@@ -10,16 +10,20 @@ function SubHeader({ onSearch }) {
     const handleSearch = (query) => {
         clearTimeout(timer); // 이전 타이머를 지움
         timer = setTimeout(() => {
-          onSearch(`대전 ${query}`);
+            onSearch(`대전 ${query}`);
         }, 3000);
-      };
-        
+    };
+
     const handleChange = (e) => {
         const query = e.target.value;
         setSearchText(query);  // 실시간으로 입력값을 업데이트
         handleSearch(query);  // 디바운싱 적용하여 검색
     };
-    
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault(); // 기본 form 제출 동작을 막음 없을 땐 엔터치면 폼이 제출돼서 페이지가 리로드 돼서 네틀리파이에서 404 페이지로 가짐. /? 로 가서.
+    };
+
     useEffect(() => {
         const handleFocus = () => setFocused(true);
         const handleBlur = () => setFocused(false);
@@ -44,13 +48,13 @@ function SubHeader({ onSearch }) {
                         </Link>
                     </div>
                     <div className='subSearch'>
-                        <form>
+                        <form onSubmit={handleFormSubmit}>
                             <i className={`fa fa-search search-icon ${isFocused ? 'focused' : ''}`}></i>
-                            <input type='text' className='search-input' 
-                            value={searchText}
-                            onChange={handleChange}
+                            <input type='text' className='search-input'
+                                value={searchText}
+                                onChange={handleChange}
                             />
-                            
+
                         </form>
                     </div>
                     <div className='subLogin'>
